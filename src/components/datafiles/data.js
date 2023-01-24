f4172df56cfacbcaa3b616a12fab13f52e4a6435
Aniket Jain <aniketjain8441@gmail.com>
2023-01-24 22:53:48 +0530

    add style, and some A data

--- src/components/datafiles/data.js
+++ src/components/datafiles/data.js
@@ -1,71 +1,71 @@
 const data = {
   a: [
     {
-      title: "appl e1",
-      desc: "appl e1",
+      title: "A.NET (A#/A sharp)",
+      desc: "A# is a port of the Ada programming language to the Microsoft .NET platform. A# is freely distributed by the Department of Computer Science at the United States Air Force Academy as a service to the Ada community under the terms of the GNU General Public License.",
     },
     {
-      title: "",
-      desc: "",
+      title: "A-0 System",
+      desc: "The A-0 system, written by Grace Murray Hopper in 1951 and 1952 for the UNIVAC I, was an early compiler related tool developed for electronic computers. The A-0 functioned more as a loader or linker than the modern notion of a compiler. A program was specified as a sequence of subroutines and its arguments. The subroutines were identified by a numeric code and the arguments to the subroutines were written directly after each subroutine code. The A-0 system converted the specification into machine code that could be fed into the computer a second time to execute the said program.",
     },
     {
-      title: "",
-      desc: "",
+      title: "A+ (A plus)",
+      desc: "A+ is a high-level, interactive, interpreted array programming language designed for numerically intensive applications, especially those found in financial applications.",
     },
     {
-      title: "",
-      desc: "",
+      title: "ABAP",
+      desc: "ABAP is a high-level programming language created by the German software company SAP SE. It is currently positioned, alongside Java, as the language for programming the SAP NetWeaver Application Server, which is part of the SAP NetWeaver platform for building business applications.",
     },
     {
-      title: "",
-      desc: "",
+      title: "ABC",
+      desc: "ABC is an imperative general-purpose programming language and integrated development environment (IDE) developed at Centrum Wiskunde & Informatica (CWI), Netherlands by Leo Geurts, Lambert Meertens, and Steven Pemberton. It is interactive, structured, high-level, and intended to be used instead of BASIC, Pascal, or AWK. It is intended for teaching or prototyping, but not as a systems-programming language.",
     },
     {
-      title: "",
-      desc: "",
+      title: "ABC ALGOL",
+      desc: "ABC ALGOL is an extension of the programming language ALGOL 60 with arbitrary data structures and user-defined operators, intended for computer algebra. Despite its advances, it was never used as widely as Algol proper.",
     },
     {
-      title: "",
-      desc: "",
+      title: "ACC",
+      desc: "ACC is a near-C compiler for the MS-DOS operating system on the IBM PC line of computers for programs. The compiler and compiled programs will run on any Intel 80386 or above PC running MS-DOS. Included with the compiler are a 386 assembler and a linker for combining multiple object files.",
     },
     {
-      title: "",
-      desc: "",
+      title: "Accent (Rational Synergy)",
+      desc: "Rational Synergy is a software tool that provides software configuration management (SCM) capabilities for all artifacts related to software development including source code, documents and images as well as the final built software executable and libraries. Rational Synergy also provides the repository for the change management tool known as Rational Change. Together these two tools form an integrated configuration management and change management environment that is used in software development organizations that need controlled SCM processes and an understanding of what is in a build of their software.",
     },
     {
-      title: "",
-      desc: "",
+      title: "Ace DASL (Distributed Application Specification Language)",
+      desc: "The DASL Programming Language (Distributed Application Specification Language) is a high-level, strongly typed programming language originally developed at Sun Microsystems Laboratories between 1999 and 2003 as part of the Ace Project. The goals of the project were to enable rapid development of web-based applications based on Sun's J2EE architecture, and to eliminate the steep learning curve of platform-specific details.",
     },
     {
-      title: "",
-      desc: "",
+      title: "Action!",
+      desc: 'Action! is a procedural programming language and integrated development environment written by Clinton Parker for the Atari 8-bit family. The language, which is similar to ALGOL, compiles to high-performance code for the MOS Technologies 6502 of the Atari computers. Action! was distributed on ROM cartridge by Optimized Systems Software starting in 1983. It was one of the company\'s first bank-switched 16 kB "Super Cartridges".',
     },
     {
-      title: "",
-      desc: "",
+      title: "ActionScript",
+      desc: "ActionScript is an object-oriented programming language originally developed by Macromedia Inc.. It is influenced by HyperTalk, the scripting language for HyperCard. It is now an implementation of ECMAScript, though it originally arose as a sibling, both being influenced by HyperTalk.",
     },
     {
-      title: "",
-      desc: "",
+      title: "Actor",
+      desc: "The Actor programming language was invented by Charles Duff of The Whitewater Group in 1988. It was an offshoot of some object-oriented extensions to the Forth language he had been working on.",
     },
     {
-      title: "",
-      desc: "",
+      title: "Ada",
+      desc: "Ada is a structured, statically typed, imperative, and object-oriented high-level programming language, extended from Pascal and other languages. It has built-in language support for design by contract (DbC), extremely strong typing, explicit concurrency, tasks, synchronous message passing, protected objects, and non-determinism.",
     },
     {
-      title: "",
-      desc: "",
+      title: "Adenine (Haystack)",
+      desc: "Haystack is a project at the Massachusetts Institute of Technology to research and develop several applications around personal information management and the Semantic Web. The most notable of those applications is the Haystack client, a research personal information manager (PIM) and one of the first to be based on semantic desktop technologies.",
     },
     {
-      title: "",
-      desc: "",
+      title: "AdvPL",
+      desc: "AdvPL is a proprietary programming language based on xBase. It was released in 1999 and is used for development of applications in the ERP Protheus made by TOTVS.",
     },
     {
-      title: "",
-      desc: "",
+      title: "Agda",
+      desc: "Agda is a dependently typed functional programming language originally developed by Ulf Norell at Chalmers University of Technology with implementation described in his PhD thesis. The original Agda system was developed at Chalmers by Catarina Coquand in 1999. The current version, originally known as Agda 2, is a full rewrite, which should be considered a new language that shares a name and tradition.",
     },
     {
-      title: "",
+      title: "Agilent VEE (Keysight VEE)",
       desc: "",
     },
     {
